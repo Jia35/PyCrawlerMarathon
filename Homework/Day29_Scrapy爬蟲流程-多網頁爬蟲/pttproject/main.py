6a137882163423e33e5cdbb7bd122fa1d487830f
Jia35 <g919233@gmail.com>
2020-07-18 14:07:28 +0800

    第二十九次作業

---
+++ Homework/Day29_Scrapy爬蟲流程-多網頁爬蟲/pttproject/main.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
+# def main():
+#     target_board = ['Gossiping', 'creditcard']
+#     process = CrawlerProcess(get_project_settings())
+#     for board in target_board:
+#         process.crawl('PTTCrawler', board=board)
+#         process.start()
+
+
+def main():
+    target_board = ['Gossiping', 'creditcard']
+    process = CrawlerProcess(get_project_settings())
+    for board in target_board:
+        process.crawl('PTTCrawler', board=board)
+    process.start()
+
+
+if __name__ == '__main__':
+    main()
